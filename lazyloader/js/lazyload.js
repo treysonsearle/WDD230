98c463221c2f18539afbd941d1670039c726ae7e
treysonsearle <treyson.searle96@gmail.com>
2021-10-28 15:05:10 -0700

    some updates

--- lazyloader/js/lazyload.js
+++ lazyloader/js/lazyload.js
@@ -2,7 +2,7 @@ const imagesToLoad = document.querySelectorAll("img[data-src]");
 
 const imgOptions = {
    threshold: 0,
-   rootMargin: "0px 0px 50px 50px"
+   rootMargin: "0px 0px 75px 0px"
 };
 
 const loadImages = (image) => {

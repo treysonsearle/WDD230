96e3dd4a526bf10afdefa33ec9b4c610fe4e9d06
treysonsearle <treyson.searle96@gmail.com>
2021-12-15 16:43:19 -0700

    link fixes

--- final/js/cards.js
+++ final/js/cards.js
@@ -1,6 +1,34 @@
-fetch('final/js/business.json')
-   .then(response => {
+requestURL = 'https://treysonsearle.github.io/final/js/business.json'
+
+fetch(requestURL)
+   .then(function (response) {
       return response.json();
-   }).then(json => {
-      console.log(json);
    })
+   .then(function (jsonObject) {
+      console.table(jsonObject);  // temporary checking for valid response and data parsing
+      const businesses = jsonObject
+
+      for (let i = 0; i < businesses.length; i++) {
+         let card = document.createElement("section");
+         let h2 = document.createElement("h2");
+         let address = document.createElement("p")
+         let number = document.createElement("p")
+         let website = document.createElement("a")
+         let logo = document.createElement("img")
+         card.setAttribute("class", `order${businesses[i].order}`)
+         h2.textContent = `${businesses[i].name}}`;
+         address.textContent = `Address: ${businesses[i].address}`;
+         number.textContent = `Phone Number: ${businesses[i].number}`;
+         website.textContent = `Website: ${businesses[i].website}`;
+         website.setAttribute("href", businesses[i].website)
+         logo.setAttribute("src", businesses[i].logo)
+         logo.setAttribute("alt", `${businesses[i].name}`)
+         card.appendChild(h2);
+         card.appendChild(address);
+         card.appendChild(number);
+         card.appendChild(website)
+         card.appendChild(logo)
+         document.querySelector('div.cards').appendChild(card)
+      }
+
+   });

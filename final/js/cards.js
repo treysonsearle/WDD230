b083675d76734508bc8e75ebf9bf49dc08be741a
treysonsearle <treyson.searle96@gmail.com>
2021-12-15 17:16:19 -0700

    grid fixes

--- final/js/cards.js
+++ final/js/cards.js
@@ -10,10 +10,11 @@ fetch(requestURL)
 
       for (let i = 0; i < businesses.length; i++) {
          let card = document.createElement("section");
-         let h2 = document.createElement("h2");
+         let h2 = document.createElement("p");
          let address = document.createElement("p")
          let number = document.createElement("p")
          let website = document.createElement("a")
+         let pic = document.createElement("a")
          let logo = document.createElement("img")
          card.setAttribute("class", `order${businesses[i].order}`)
          h2.textContent = `${businesses[i].name}}`;
@@ -21,13 +22,16 @@ fetch(requestURL)
          number.textContent = `Phone Number: ${businesses[i].number}`;
          website.textContent = `Website: ${businesses[i].website}`;
          website.setAttribute("href", businesses[i].website)
+         website.classList.add('link')
          logo.setAttribute("src", businesses[i].logo)
          logo.setAttribute("alt", `${businesses[i].name}`)
+         pic.setAttribute("href", businesses[i].website)
+         pic.appendChild(logo)
          card.appendChild(h2);
          card.appendChild(address);
          card.appendChild(number);
          card.appendChild(website)
-         card.appendChild(logo)
+         card.appendChild(pic)
          document.querySelector('div.cards').appendChild(card)
       }
 

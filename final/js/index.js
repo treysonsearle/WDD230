369e568732dd9c810eb61be493d08f8ba7a46006
treysonsearle <treyson.searle96@gmail.com>
2021-12-15 19:08:11 -0700

    FINAL CHANGES

--- final/js/index.js
+++ final/js/index.js
@@ -1,3 +1,13 @@
 function toggleMenu() {
    document.getElementsByClassName("menu")[0].classList.toggle("responsive");
 };
+
+
+const options = {
+   weekday: 'long',
+   day: 'numeric',
+   month: 'long',
+   year: 'numeric'
+};
+let oLastModif = new Date(document.lastModified);
+document.getElementById("current-date").textContent = oLastModif.toLocaleDateString('en-uk', options);
